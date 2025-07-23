6fdf171bdcde64fc3600663ca6f500b6430c3eba
anon.js <65931804+anon-js@users.noreply.github.com>
2025-07-23 20:17:24 +0900

    Add: required comment.

--- app/page.tsx
+++ app/page.tsx
@@ -3,3 +3,109 @@ export default function Home() {
     <>Hello world :)</>
   );
 }
+
+/*<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <!-- Meta Tag -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Title" content="anon" />
+        <meta name="Description" content="i'm anon!" />
+        <meta http-equiv="Author" content="anon" />
+        <meta http-equiv="publisher" content="anon" />
+        <meta http-equiv="Other Agent" content="anon" />
+        <meta http-equiv="Reply-To" content="anon@qnon.kr" />
+        <meta http-equiv="Email" content ="anon@qnon.kr" />
+        <meta name="keywords" content="anon">
+        <meta name="keywords" content="frontend">
+        <meta http-equiv="Subject" content="portfoilo" />
+        
+        <!-- Open Graph Tag -->
+        <meta property="og:url" content="https://qnon.kr" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="anon" />
+        <meta property="og:title" content="anon" />
+        <meta property="og:description" content="i'm anon!" />
+        
+        <title>anon</title>
+        
+        <style type="text/css">
+            @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+            
+            * {
+                -webkit-user-select: none;
+                user-select: none;
+                word-break: keep-all;
+                font-family: -apple-system, BlinkMacSystemFont, 'Apple SD Gothic Neo', Pretendard, Roboto, 'Noto Sans KR', 'Segoe UI', 'Malgun Gothic', sans-serif;
+            }
+            
+            html, body {
+                margin:  0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                color: #1d1d1f;
+            }
+            
+            header {
+                width: 100%;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                gap: 16px;
+            }
+            
+            header > h4 {
+                margin: 12px;
+            }
+            
+            header > ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                display: flex;
+                flex-direction: row;
+                gap: 16px;
+            }
+            
+            header > ul > li > a {
+                text-decoration: none;
+                color: #202022;
+            }
+            
+            footer {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+                color: #7f7f8b;
+                padding: 12px 0;
+            }
+        </style>
+    </head>
+        <body>
+            <header>
+                <h4>qnon.kr</h4>
+                <ul>
+                    <li><a href="#about">소개</a></li>
+                    <li><a href="#skills">기술</a></li>
+                    <li><a href="#edu">학력 및 자격증</a></li>
+                    <li><a href="#project">프로젝트</a></li>
+                </ul>
+            </header>
+            <main>
+                <section id="main"></section>
+                <section id="about"></section>
+                <section id="skills"></section>
+                <section id="edu"></section>
+                <section id="project"></section>
+            </main>
+            <footer>
+                &copy; 2025 anon.
+            </footer>
+        </body>
+</html>*/
