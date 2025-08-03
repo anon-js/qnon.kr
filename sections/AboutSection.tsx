6c3964998cbba8331c3763967ed0ca30358c47ac
anon.js <65931804+anon-js@users.noreply.github.com>
2025-08-03 14:44:46 +0000

    Fix: build error

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -4,7 +4,7 @@ export default function AboutSection() {
   return (
     <Section
       title="현재 웹사이트를 개발 중에 있습니다."
-      descriptionText={
+      description={
         <>
           이 부분은 소개에 속하는 부분입니다.<br />
           사용자 경험을 위해 많이 노력하고 있어요.<br />
