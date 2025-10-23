dc52c1e6ead1c066cfe491143617cf6b56b167d4
anon.js <65931804+anon-js@users.noreply.github.com>
2025-10-23 14:56:54 +0000

    initial settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,16 @@
 import type { Metadata } from "next";
-import "@/styles/globals.css";
-import "@/styles/fonts.css";
-import { header, logoLink, nav, navLink, footer } from "./layout.css";
-import Link from "next/link";
+import localFont from "next/font/local";
+import "./globals.css";
 
-// Meta Tag
-// <meta charset="utf-8">
-// <meta name="viewport" content="width=device-width, initial-scale=1.0">
-// <meta http-equiv="Title" content="anon" />
-// <meta name="Description" content="i'm anon!" />
-// <meta http-equiv="Author" content="anon" />
-// <meta http-equiv="publisher" content="anon" />
-// <meta http-equiv="Other Agent" content="anon" />
-// <meta http-equiv="Reply-To" content="anon@qnon.kr" />
-// <meta http-equiv="Email" content ="anon@qnon.kr" />
-// <meta name="keywords" content="anon">
-// <meta name="keywords" content="frontend">
-// <meta http-equiv="Subject" content="portfoilo" />
-
-// Open Graph Tag
-// <meta property="og:url" content="https://qnon.kr" />
-// <meta property="og:type" content="website" />
-// <meta property="og:site_name" content="anon" />
-// <meta property="og:title" content="anon" />
-// <meta property="og:description" content="i'm anon!" />
+const pretendard = localFont({
+  src: "../public/fonts/PretendardVariable.woff2",
+  variable: "--font-pretendard",
+  display: "swap"
+});
 
 export const metadata: Metadata = {
   title: "anon",
-  description: "I'm anon!",
+  description: "anon's portfolio website",
 };
 
 export default function RootLayout({
@@ -37,22 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        <header className={header}>
-          <h4>
-            <a href="../" className={logoLink}>qnon.kr</a>
-          </h4>
-          <nav className={nav}>
-              <Link href="../" className={navLink}>홈</Link>
-              <Link href="./exp" className={navLink}>경험</Link>
-              <Link href="./project" className={navLink}>프로젝트</Link>
-              <Link href="./contact" className={navLink}>연락</Link>
-          </nav>
-        </header>
+      <body className={`${pretendard.className}`}>
         {children}
-        <footer className={footer}>
-          &copy; 2025 anon.
-        </footer>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,60 @@
-import MainSection from '@/sections/MainSection';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardAction,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import { IconBrandGithub } from "@tabler/icons-react"
+import { ArrowUpRightIcon, MailIcon } from "lucide-react"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
-      <MainSection />
-    </main>
-  );
+    <div className="flex flex-col items-center justify-center h-screen p-4 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>anon</CardTitle>
+          <CardDescription>프론트엔드 개발자</CardDescription>
+          <CardAction className="flex gap-2">
+            <Button asChild>
+              <Link href="mailto:anon@qnon.kr" target="_blank" rel="noopener noreferrer">
+                <MailIcon /> Email
+              </Link>
+            </Button>
+            <Button asChild>
+              <Link href="https://github.com/anon-js" target="_blank" rel="noopener noreferrer">
+                <IconBrandGithub /> GitHub
+              </Link>
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <h1 className="text-xl text-gray-900 font-bold">
+            모두에게 동일한 경험을
+          </h1>
+          <p className="mt-2 text-base text-gray-600">
+            사용자가 어떤 조건에 놓여있든지 상관없이 동일한 경험을 제공하는 서비스를 만들고 싶습니다.
+          </p>
+        </CardContent>
+        <CardFooter>
+          <Button variant="link" className="cursor-not-allowed" disabled>
+            프로젝트 <ArrowUpRightIcon />
+          </Button>
+          <Button variant="link" className="cursor-not-allowed" disabled>
+            연혁 <ArrowUpRightIcon />
+          </Button>
+        </CardFooter>
+      </Card>
+      <footer>
+        <p className="text-sm text-gray-500 text-center">
+          made by anon
+        </p>
+      </footer>
+    </div>
+  )
 }
